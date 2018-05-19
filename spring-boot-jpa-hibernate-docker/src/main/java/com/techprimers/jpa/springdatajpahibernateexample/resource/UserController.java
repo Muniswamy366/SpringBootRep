a13616207f9408329e8ded7d956d891d986cb76a
Muni <pallamuniswamy@gmail.com>
2018-05-19 08:13:30 +0530

    done

--- spring-boot-jpa-hibernate-docker/src/main/java/com/techprimers/jpa/springdatajpahibernateexample/resource/UserController.java
+++ spring-boot-jpa-hibernate-docker/src/main/java/com/techprimers/jpa/springdatajpahibernateexample/resource/UserController.java
@@ -2,7 +2,9 @@ package com.techprimers.jpa.springdatajpahibernateexample.resource;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,12 +25,15 @@ public class UserController {
 
 	@PostMapping("/add")
 	public List<Users> users(@RequestBody Users users) {
-		/*
-		 * Users users = new Users(); users.setId(1); users.setName("Sam");
-		 * users.setSalary(3400);
-		 */
-
 		usersRepository.save(users);
+
+		return usersRepository.findAll();
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public List<Users> deleteUser(@RequestBody Users users, @PathVariable("id") Integer id) {
+
+		usersRepository.delete(id);
 
 		return usersRepository.findAll();
 	}
