a7f57a305d7b13d385498581dffa7eeab6b3966e
Muni <Muni@DESKTOP-OKM9TUC>
2018-11-18 15:15:00 +0530

    first

---
+++ spring-boot-hello-world/src/main/java/com/arya/helloworld/SpringBootHelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.arya.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+	}
+	
+	@GetMapping(path="/hello")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
